8f30a3e5a00034a496653da82d201e17b85f156e
Araki <s1550002@jaist.ac.jp>
2016-05-09 04:04:37 +0900

    a

--- app/program.py
+++ app/program.py
@@ -2,15 +2,29 @@
 # -*- coding: utf-8 -*-
 
 import random
+import csv
 
 class Program:
+  def dp(self):
+      for i, a in enumerate(self.memo):
+          for j, v in enumerate(a):
+              c = self.rows[i][0]
+              x = self.rows[i][1]
+              if i >= (self.memo[i-1][j][0] + cost):
+                  self.memo[i][j] = self.memo[i-1][j][0] + cost
+              else :
+                  self.memo[i][j] = self.memo[i][j-1]
+              self.memo[i][j] = self.memo[i][j] if vij[1] > self.memo[i-1][j][1]
+
   def build_deck(self, filename):
     # デッキを構築する関数
     # 引数 filename はカードのデータファイル
     # 返り値は文字列
-    file = open(filename);
-    print file.read()
-    print file.read()
+    self.filename = filename
+    self.memo = [[[0, ""] for _ in enumerate(10001)] for _ in enumerate(40001)]
+    csvfile = open(filename)
+    reader = csv.reader(csvfile)
+    self.rows = [[row[0], row[1]] for row in reader]
     return "00000 00001 ..."
 
   def gacha(self, user_id):
